8bc7ff7a421ec787fadb6a8c22d25cf8f908dadd
unknown <shoon2430@gmail.com>
2020-03-02 22:53:07 +0900

    fixed setting.py

--- config/settings.py
+++ config/settings.py
@@ -148,8 +148,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(DJANGO_DIR, 'static'),
+]
 
 AUTH_USER_MODEL = "users.User"
 
