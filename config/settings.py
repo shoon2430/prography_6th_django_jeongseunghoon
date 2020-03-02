9f4aeeb51b2dda76bbdfded1f1fe4cfd776815bd
unknown <shoon2430@gmail.com>
2020-03-02 17:29:26 +0900

    fixed settins.py

--- config/settings.py
+++ config/settings.py
@@ -151,6 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 AUTH_USER_MODEL = "users.User"
 
